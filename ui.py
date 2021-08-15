6cfb625d22b36ff55524100d0ced880f65503e09
Shuvo Nasir <shuvonasir028@gmail.com>
2021-08-16 00:52:01 +0600

    create ui for the app

--- ui.py
+++ ui.py
@@ -1,2 +1,57 @@
-THEME_COLOR = "#375362"
+from tkinter import *
+from quiz_brain import QuizBrain
 
+THEME_COLOR = "#375362"
+FONT = ("Arial", 20, "italic")
+
+
+class QuizInterface:
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
+        # Window
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(bg=THEME_COLOR, padx=20, pady=20)
+        self.window.resizable(width=False, height=False)
+
+        # Score label
+        self.score = 0
+        self.score_label = Label(text=f"Score: {self.score}", fg="white", bg=THEME_COLOR, highlightthickness=0)
+        self.score_label.grid(row=0, column=1)
+
+        # Canvas
+        self.canvas = Canvas()
+        self.canvas.config(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            width=280,
+            text="",
+            font=FONT,
+            fill=THEME_COLOR
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        # Buttons
+        true_button_img = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_button_img, highlightthickness=0, command=self.pressed_true)
+        self.true_button.grid(row=2, column=0)
+
+        false_button_img = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_button_img, highlightthickness=0, command=self.pressed_false)
+        self.false_button.grid(row=2, column=1)
+
+        self.get_next_question()
+
+        self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
+
+    def pressed_true(self):
+        self.quiz.check_answer(user_answer="True")
+
+    def pressed_false(self):
+        self.quiz.check_answer(user_answer="False")
