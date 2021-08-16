1516bfab8287d99e0cc66f43e25a7663f90d5b1e
Shuvo Nasir <shuvonasir028@gmail.com>
2021-08-17 00:11:58 +0600

    finalize the project

--- ui.py
+++ ui.py
@@ -2,6 +2,8 @@ from tkinter import *
 from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
+RIGHT_ANS_COLOR = "#27ae60"
+WRONG_ANS_COLOR = "#ff6b6b"
 FONT = ("Arial", 20, "italic")
 
 
@@ -15,8 +17,14 @@ class QuizInterface:
         self.window.config(bg=THEME_COLOR, padx=20, pady=20)
         self.window.resizable(width=False, height=False)
 
+        # Question number label
+        self.question_number = self.quiz.question_number
+        self.question_number_label = Label(text=f"Question: {self.question_number}/{len(self.quiz.question_list)}",
+                                           fg="White", bg=THEME_COLOR, highlightthickness=0)
+        self.question_number_label.grid(row=0, column=0)
+
         # Score label
-        self.score = 0
+        self.score = self.quiz.score
         self.score_label = Label(text=f"Score: {self.score}", fg="white", bg=THEME_COLOR, highlightthickness=0)
         self.score_label.grid(row=0, column=1)
 
@@ -47,11 +55,33 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.question_number += 1
+            self.question_number_label.config(text=f"Question: {self.question_number}/{len(self.quiz.question_list)}",
+                                              fg="White", bg=THEME_COLOR, highlightthickness=0)
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text, fill=THEME_COLOR)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz!", fill=THEME_COLOR)
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
 
     def pressed_true(self):
-        self.quiz.check_answer(user_answer="True")
+        is_right = self.quiz.check_answer(user_answer="True")
+        self.give_feedback(is_right)
 
     def pressed_false(self):
-        self.quiz.check_answer(user_answer="False")
+        is_right = self.quiz.check_answer(user_answer="False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg=RIGHT_ANS_COLOR)
+            self.canvas.itemconfig(self.question_text, fill="white")
+            self.score += 1
+            self.score_label.config(text=f"Score: {self.score}")
+        else:
+            self.canvas.config(bg=WRONG_ANS_COLOR)
+            self.canvas.itemconfig(self.question_text, fill="white")
+        self.window.after(1000, self.get_next_question)
